ca81c8e5019cfe6cee3090a16e5674ce354a1b6e
ChilliRoger <155988166+ChilliRoger@users.noreply.github.com>
2025-06-09 21:35:23 +0530

    06-09-2025 0440-k-th-smallest-in-lexicographical-order

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.java
@@ -1,29 +1,29 @@
 class Solution {
-    public int findKthNumber(int n, int k) {
-        int current = 1;
-        k--;
-        
-        while (k > 0) {
-            int count = countSteps(n, current, current + 1);
-            if (count <= k) {
-                current++;
-                k -= count;
-            } else {
-                current *= 10;
-                k--;
-            }
-        }
-        
-        return current;
+     static public int findKthNumber(int n, int K) {
+        long k = K;
+        int ans = solve(0, n, k);
+        return ans;
     }
 
-    private int countSteps(int n, int curr, int next) {
-        int steps = 0;
-        while (curr <= n) {
-            steps += Math.min(n + 1, next) - curr;
-            curr *= 10;
-            next *= 10;
+    static int solve(long current, long n, long k) {
+        if(k == 0) 
+             return (int) (current / 10);
+
+        for (long i = Math.max(current, 1); i < current + 10; i++) {
+            long count = numOfChildren(i, i + 1, n);
+            if (count >= k) 
+                return solve(i * 10, n, k - 1);
+            k-= count;
         }
-        return steps;
+
+        return -1;
+    }
+
+    static long numOfChildren(long current, long neighbour, long n) {
+        if (neighbour > n) {
+            if (current > n) return 0;
+            return n - current + 1;
+        }
+        return neighbour - current + numOfChildren(current * 10, neighbour * 10, n);
     }
 }
