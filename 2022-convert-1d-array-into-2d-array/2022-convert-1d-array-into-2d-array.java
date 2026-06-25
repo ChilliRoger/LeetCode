4cd2c87cc2e077be8048613a65297b35f19e3b27
ChilliRoger <155988166+ChilliRoger@users.noreply.github.com>
2026-06-25 12:27:50 +0530

    06-25-2026 2022-convert-1d-array-into-2d-array

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
@@ -4,7 +4,6 @@ class Solution {
         if(m*n!=original.length){
             return new int[0][0];
         }
-        else{
             int c=0;
             for (int i=0; i<m;i++){
                 for (int j=0;j<n;j++){
@@ -13,6 +12,5 @@ class Solution {
                 }
             }
             return arr;
-        }
     }
 }
