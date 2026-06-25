628377a81c3e2bac415c1067cdb41a5b6db5fe2f
ChilliRoger <155988166+ChilliRoger@users.noreply.github.com>
2026-06-25 11:12:35 +0530

    06-25-2026 0048-rotate-image

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,13 +1,24 @@
 class Solution {
-    public void rotate(int[][] mat) {
-        int n = mat.length, k = n - 1;
-        for (int i = 0; i < n >> 1; i++)
-            for (int j = i; j < k - i; j++) {
-                int t = mat[i][j];
-                mat[i][j] = mat[k - j][i];
-                mat[k - j][i] = mat[k - i][k - j];
-                mat[k - i][k - j] = mat[j][k - i];
-                mat[j][k - i] = t;
+    public void rotate(int[][] matrix) {
+        for (int i=0;i<matrix.length;i++){
+            for(int j=i+1;j<matrix.length;j++){
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
-    }
-}
+        }
+
+        for(int k=0;k<matrix.length;k++){
+            int start =0;
+            int end= matrix.length-1;
+            while(start<end){
+            int temp=matrix[k][start];
+            matrix[k][start]=matrix[k][end];
+            matrix[k][end]=temp;
+            start++;
+            end--;
+        }
+
+     }
+     }
+ }
