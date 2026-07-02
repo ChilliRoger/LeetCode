823478d3fd241e9b2b37f14d11d8edbbab11c966
ChilliRoger <155988166+ChilliRoger@users.noreply.github.com>
2026-07-02 10:47:21 +0530

    07-02-2026 0242-valid-anagram

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()) return false;
         char[] cs = s.toCharArray();
         char[] ct = t.toCharArray();
         Arrays.sort(cs);
