e383675b58524348bf16136e4617a531d0b03b6c
ChilliRoger <155988166+ChilliRoger@users.noreply.github.com>
2026-07-03 08:59:07 +0530

    07-03-2026 0191-number-of-1-bits

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,12 +1,10 @@
 class Solution {
     public int hammingWeight(int n) {
-        int count=0;
-        while(n!=0){
-            if(n%2==1){
-                count++;
-            }
-                n=n>>1;
-
+        int count = 0 ;
+        while(n>0){
+            if((n&1)!=0)
+            count++;
+         n/=2;
         }
         return count;
     }
