2d85f30b2545a84028fbf1c7264391404199e32a
ChilliRoger <155988166+ChilliRoger@users.noreply.github.com>
2026-07-03 09:25:21 +0530

    07-03-2026 0231-power-of-two

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,6 +1,10 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
         if(n<=0) return false;
-        return (n&(n-1))==0;
+        while(n%2==0){
+            n=n/2;
+        }
+        if(n==1) return true;
+        else return false;
     }
 }
