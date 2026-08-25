d6b7f8e756ea9a0f4e0ba3deebe6134229921d01
ChilliRoger <155988166+ChilliRoger@users.noreply.github.com>
2026-08-25 08:57:33 +0530

    08-25-2026 3718-smallest-missing-multiple-of-k

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        int n=nums.length;
+        HashSet<Integer> set = new HashSet<>();
+        for(int num:nums){
+            set.add(num);
+        }
+        for(int i=1;i<=n+1;i++){
+            if(!set.contains(k*i))
+            return (k*i);
+        }
+  return k*2;  }
+}
